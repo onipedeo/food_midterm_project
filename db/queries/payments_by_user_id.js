1826a1bad65b1d8951b9100e7aa4635042da15aa
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-08-15 22:02:45 -0400

    get payment information by user_id.

---
+++ db/queries/payments_by_user_id.js
@@ -0,0 +1,14 @@
+const db = require('../connection');
+
+const getPaymentsByUserId = (userId) => {
+  return db.query(`
+  SELECT *
+  FROM payment_details
+  WHERE user_id = $1;`
+  , [userId])
+    .then(data => {
+      return data.rows;
+    });
+};
+
+module.exports = { getPaymentsByUserId };
