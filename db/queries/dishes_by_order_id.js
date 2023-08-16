8359643f43ee9bbd7c053fdd1bca0bb8fe2d4614
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-08-15 22:30:38 -0400

    Add documentation

--- db/queries/dishes_by_order_id.js
+++ db/queries/dishes_by_order_id.js
@@ -1,10 +1,15 @@
 const db = require("../connection");
-
+/**
+ *
+ * @param {idNum} orderId
+ * @returns {Promise} a promise to the dishes in the order
+ * @contains {dishName, dishCount, dishPrice}
+ */
 const getDishesByOrder = (orderId) => {
   return db
     .query(
       `
-      SELECT dishes.name as dish_name, count(dishes.id) AS dish_count, dishes.price AS price
+      SELECT dishes.name as dishName, count(dishes.id) AS dishCount, dishes.price AS dishPrice
       FROM orders_dishes
       JOIN dishes ON orders_dishes.dish_id = dishes.id
       WHERE order_id = $1
--- db/queries/menu.js
+++ db/queries/menu.js
@@ -1,5 +1,10 @@
 const db = require('../connection');
-
+/**
+ * @param {none}
+ * @returns {Promise<{}>} A promise to the menu
+ *
+ * @contains {id, name, description, price, vegan, image_url}
+ */
 const getMenu = () => {
   return db.query(`SELECT * FROM dishes;`)
     .then(data => {
