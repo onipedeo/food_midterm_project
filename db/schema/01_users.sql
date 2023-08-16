d8f8f54a5af49e6440701be52b643eb39e8c1365
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-08-15 23:52:17 -0400

    fix bugs in database setup

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -3,7 +3,7 @@ CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
-  phone_number INTEGER NOT NULL,
+  phone_number BIGINT NOT NULL,
   password VARCHAR(255),
   is_employee BOOLEAN DEFAULT FALSE
 );
--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,3 +1,3 @@
 -- Users table seeds here (Example)
-INSERT INTO users (name, email, phone_number, password, is_employee) VALUES ('Alice', alice@employee.ca, 3433948173, password, true);
-INSERT INTO users (name, email, phone_number, password) VALUES ('Kira', kira@customer.ca, 6138998878, password);
+INSERT INTO users (name, email, phone_number, password, is_employee) VALUES ('Alice', 'alice@employee.ca', 3433948173, 'password', true);
+INSERT INTO users (name, email, phone_number, password) VALUES ('Kira', 'kira@customer.ca', 6138998878, 'password');
