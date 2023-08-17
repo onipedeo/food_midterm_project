6fe3a46289151048a13d426ed33fae40c3b3b5b8
carolineolagunju <olamidejr21@gmail.com>
2023-08-17 17:33:41 -0600

    deleted hard coded html

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,7 +1,4 @@
 $(document).ready(function(){
-  // $.get('/api/widgets', (data) => {
-  //   console.log("data", data);
-  // });
 
   $.ajax({
     url: "/api/widgets",
@@ -21,35 +18,6 @@ $(document).ready(function(){
   });
 
   function displayDishes(item){
-
-        // let menuHtml = `` // string to concat to. will hold the resulting html.
-        // // html template for menu item
-        // // loop through items and add to menuHtml
-        //   const price = item.price;
-        //   const image_url = item.image_url;
-        //   const pizza_name = item.name;
-        //   const description = item.description;
-        //   const isVegan = item.vegan;
-          
-        //   const itemHtml = `
-        //   <article class="menu_item">
-        //   <img class="pizza_image" src="${image_url}" alt="pizza picture">
-        //     <span>
-        //       <h3 class="pizza_title">${pizza_name}</h3> 
-        //       <p class="pizza_description">${description}</p>
-        //       <div>vegan: ${isVegan ? "yes" : "no"}</div> 
-        //     </span>
-        //     <div>
-        //       <h6>Price: ${price}</h6>
-        //     </div>
-        //   </article>
-        //   `; 
-        //   menuHtml += itemHtml;
-        //   return menuHtml;
-
-        // string to concat to. will hold the resulting html.
-        // html template for menu item
-        // loop through items and add to menuHtml
           const price = item.price;
           const image_url = item.image_url;
           const pizza_name = item.name;
