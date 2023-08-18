ae8b6abb7e05f61c5e50365209c71ec16bea62e0
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-08-18 00:03:51 -0400

    add addItemToCart function, and button listener adjust menuHtml classes.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,31 +1,56 @@
-$(document).ready(function(){
+$(document).ready(function () {
+  console.log("loaded")
+  // create local cart object
+  const cart = {};
 
+  const addToCartListener = () => {
+    $(".add_to_order").on("click", function () {
+      const $itemName = $(this)
+      .closest(".menu_item")
+      .find(".pizza_title")
+      .text();
+      
+      let $itemPrice = $(this)
+      .closest(".menu_item")
+      .find(".price").
+      text()
+      //$itemPrice = $itemPrice.split(" ")[1];
+      
+      console.log($(this).closest(".menu_item"));
+      addItemToCart($itemName, $itemPrice);
+      console.log('add to order was clicked');
+    })
+  };
+
+  const render_menu = function (result) {
+    //Here we are getting all the data. Now we can create function to display the data in the html over here.
+    for (let item of result.dish) {
+      let returnItem = displayDishes(item);
+      $("#menu").append(returnItem);
+      
+    };
+  };
   $.ajax({
     url: "/api/widgets",
     method: "GET",
-    success: function(result){
-      console.log("Dishes Data ",result);
-      //Here we are getting all the data. Now we can create function to display the data in the html over here.
-      for(let item of result.dish){
-        let returnItem = displayDishes(item);
-        $('#menu').append(returnItem);
-      }
-      //displayDishes(result.dish);
+    success: (result) => {
+      render_menu(result);
+      addToCartListener();
     },
-    error: function (err){
+    error: function (err) {
       console.log("Error in Fetch Dishes Data ", err);
-    }
+    },
   });
 
-  function displayDishes(item){
-          const price = item.price;
-          const image_url = item.image_url;
-          const pizza_name = item.name;
-          const description = item.description;
-          const isVegan = item.vegan;
+  function displayDishes(item) {
+    const price = item.price;
+    const image_url = item.image_url;
+    const pizza_name = item.name;
+    const description = item.description;
+    const isVegan = item.vegan;
 
-          const menuHtml = `
-          <article class="menu_item" id="container">
+    const menuHtml = `
+          <article class="menu_item container">
           <img class="pizza_image" src="${image_url}" alt="pizza picture">
           <div class="pizza_text">
             <span>
@@ -33,14 +58,46 @@ $(document).ready(function(){
               <p class="pizza_description">${description}</p>
               <p>Vegan: ${isVegan ? "Yes" : "No"}</p>
             </span>
-            <div>
-              <h6>Price: ${price}</h6>
+            <div class="price">
+              Price: ${price}
             </div>
-            </div>
-            <button class="btn btn-warning btn-lg" id="place_order">Add to Order</button>
+          </div>
+            <button class="btn btn-warning btn-lg add_to_order">Add to Order</button>
           </article>
           `;
-          return menuHtml;
+    return menuHtml;
+  }
+
+  const addItemToCart = (itemName, itemPrice) => {
+      console.log(itemName, itemPrice);
+      const item = {
+      name: itemName,
+      price: itemPrice,
+      quantity: 0
+    };
+    /*if (cart[itemName]) {
+      cart[itemName].quantity += 1;
+      $(itemName)
+        .find(".cart-item-quantity")
+        .text(cart[itemName].quantity);
+      $(itemName)
+        .find(".cart-item-price")
+        .text(`$${item.price * cart[itemName].quantity}`);
+    } else { */
+      cart[itemName] = item;
+      cart[itemName].quantity = 1;
+      itemHtml = `
+  <div class="cart-item" id=${itemName}>
+    <span class="cart-item-name">${itemName}</span>
+    <span class="cart-item-quantity">${item.quantity}<span>
+    <span class="cart-item-price">${itemPrice}</span>
+  </div>
+  `;
+      console.log(itemHtml);
+      $("#cart").append(itemHtml);
+   // }
   };
 
-})
+  
+
+});
