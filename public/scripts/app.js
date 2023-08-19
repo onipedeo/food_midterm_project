406e7d0ea8548d35be8698959d2e847448122c8c
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-08-19 19:15:04 -0400

    remove cart object from app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,16 +2,9 @@ import { functions } from './functions/functions_index.js';
 import { listeners } from './listeners/listeners_index.js';
 
 
-
-import { viewOrderListener } from './listeners/view_order_listener.js'
 $(document).ready(function() {
 
   listeners.viewOrderListener();
-
-
-  // create local cart object
-  const cart = {};
-
 
   $.ajax({
     url: "/api/widgets",
