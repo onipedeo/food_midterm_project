028226b81b613ca2ae5a4a2fb9ffb0ea1561f784
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-08-19 19:53:03 -0400

    total order using jquery

--- public/scripts/functions/order_total.js
+++ public/scripts/functions/order_total.js
@@ -1,18 +1,25 @@
 // total prices of items in cart
 export const orderTotal = () => {
   let total = 0;
-  const $cart = $("#cart")
+  const $cart = $("#cart");
 
-  $cart.children().each(function() {
+  $cart.children().each(function () {
     let price = $(this).children(".cart-item-price").text();
-    price = Number(price.slice(3));
+
+    //format price to number
+    price = Number(
+      price
+        .split(" ")
+        .filter((x) => x !== "")[3]
+        .slice(1)
+    );
+
     let quantity = $(this).children(".cart-item-quantity").text();
-    let itemTotal = (price) * Number(quantity);
-    total += Number(Math.floor(itemTotal * 100) / 100);
+
+    let itemTotal = price * Number(quantity);
+
+    total += Math.floor(itemTotal * 100) / 100;
   });
-  console.log(total);
 
   return total;
 };
-
-
