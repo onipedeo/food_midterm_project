57bd61fa293b54f95038fe96668c9ccc3459cc4c
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-08-20 17:04:35 -0400

    J-Query for handling login and show different element for customer and employee

--- public/scripts/users.js
+++ public/scripts/users.js
@@ -1,18 +1,33 @@
-// Client facing scripts here
+console.log("Document ready");
 
-$(() => {
-  $('#fetch-users').on('click', () => {
-    $.ajax({
-      method: 'GET',
-      url: '/api/users'
-    })
-    .done((response) => {
-      const $usersList = $('#users');
-      $usersList.empty();
+$(document).ready(function() {
+  $("#login-form").submit(function(event) {
+    event.preventDefault();
 
-      for(const user of response.users) {
-        $(`<li class="user">`).text(user.name).appendTo($usersList);
+    const email = $("#email").val();
+    const password = $("#password").val();
+
+    $.post("/api/logins", { email: email, password: password }, function(response) {
+      const user = response.user;
+
+      if (user.is_employee) {
+        $(".username-display").text(`Welcome, ${user.name}. You are logged in as an employee.`);
+        $(".customer-content").hide();
+        $(".employee-content").show();
+
+        // Assign the user ID to a cookie using js-cookie
+        Cookies.set("employeeID", user.id);
+      } else {
+        $(".username-display").text(`Welcome, ${user.name}. Enjoy!`);
+        $(".employee-content").hide();
+        $(".customer-content").show();
       }
+      // Clear the email and password input fields
+      $("#email").val("");
+      $("#password").val("");
+    })
+    .fail(function(error) {
+      console.error("error occurred:", error);
     });
   });
 });
