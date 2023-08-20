a7f5145e45b1dbeb5b4d1129a56e47af967ea0d7
carolineolagunju <olamidejr21@gmail.com>
2023-08-20 16:00:15 -0600

    add checkout route

--- server.js
+++ server.js
@@ -30,6 +30,7 @@ app.use(express.static('public'));
 // Note: Feel free to replace the example routes below with your own
 const userApiRoutes = require('./routes/users-api');
 const widgetApiRoutes = require('./routes/widgets-api');
+const checkoutApiRoutes = require('./routes/checkout-api');
 // const usersRoutes = require('./routes/users');
 //const orderApi = require('./routes/orders-api');
 
@@ -38,6 +39,7 @@ const widgetApiRoutes = require('./routes/widgets-api');
 // Note: Endpoints that return data (eg. JSON) usually start with `/api`
 app.use('/api/users', userApiRoutes);
 app.use('/api/widgets', widgetApiRoutes);
+app.use('/api/checkout/', checkoutApiRoutes);
 // app.use('/users', usersRoutes);
 //app.use('/api/orders', orderApi);
 // Note: mount other resources here, using the same pattern above
