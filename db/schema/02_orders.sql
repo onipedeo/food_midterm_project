66b3299c26265e38622591262ca77a16c5309cf6
carolineolagunju <olamidejr21@gmail.com>
2023-08-21 16:14:25 -0600

    clear cart, and display success msg

--- db/schema/02_orders.sql
+++ db/schema/02_orders.sql
@@ -2,7 +2,7 @@ DROP TABLE IF EXISTS orders CASCADE;
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INT REFERENCES users,
-  orders_date DATE NOT NULL DEFAULT CURRENT_DATE,
+  orders_date DATE NOT NULL DEFAULT NOW(),
   orders_total INT NOT NULL,
   order_complete BOOLEAN NOT NULL DEFAULT FALSE
 );
