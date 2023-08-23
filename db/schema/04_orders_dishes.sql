30d6e8158107088dc506c3a6619500e1f62cb3ca
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-08-23 17:17:26 -0400

    add estimated_completion column to orders_dishes

--- db/schema/04_orders_dishes.sql
+++ db/schema/04_orders_dishes.sql
@@ -2,5 +2,6 @@ DROP TABLE IF EXISTS orders_dishes CASCADE;
 CREATE TABLE orders_dishes (
   order_id INT REFERENCES orders(id) ON DELETE CASCADE,
   dish_id INT REFERENCES dishes(id) ON DELETE CASCADE,
-  quantity INT NOT NULL DEFAULT 0
+  quantity INT NOT NULL DEFAULT 0,
+  estimated_completion TIME
 );
