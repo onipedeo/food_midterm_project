b91d84635c53ce16e1657519f32e458d06e53d24
carolineolagunju <olamidejr21@gmail.com>
2023-08-22 22:12:33 -0600

    add a placeholder for estimated delivery time

--- public/scripts/listeners/checkout_listener.js
+++ public/scripts/listeners/checkout_listener.js
@@ -1,63 +1,69 @@
-import { functions } from '../functions/functions_index.js';
-import { listeners } from '../listeners/listeners_index.js';
+import { functions } from "../functions/functions_index.js";
 
 export const checkoutListener = () => {
-  $(".checkout-button").on("click", function() {
+  $(".checkout-button").on("click", function () {
+    const orderDetails = functions.getOrderDetailsFromPage();
+    const orderTotal = functions.orderTotal();
 
-  const orderDetails = functions.getOrderDetailsFromPage();
-  const orderTotal = functions.orderTotal();
+    const orderData = {
+      userId: 2,
+      orderTotal: orderTotal,
+      orderDetails: JSON.stringify(orderDetails), // Convert to JSON string
+    };
 
-  const orderData = {
-    userId: 2,
-    orderTotal: orderTotal,
-    orderDetails: JSON.stringify(orderDetails) // Convert to JSON string
-  };
+    $.ajax({
+      url: "/api/checkout/submit-order",
+      method: "POST",
+      data: orderData,
+      success: function (response) {
+        console.log("Order submitted:", response);
 
-  
-  $.ajax({
-    url: "/api/checkout/submit-order",
-    method: "POST",
-    data: orderData,
-    success: function(response) {
-      console.log("Order submitted:", response);
-      
-      $.ajax({
-        url: "/api/checkout/send-twilio-text",
-        method: "POST",
-        data: {orderDetails, orderTotal},
-        success: function(response) {
-          console.log("Twilio text sent:", response);
-        },
-        error: function(error) {
-          console.error("Error sending Twilio text:", error);
-        }
-      });
-    },
-    error: function(error) {
-      console.error("Error submitting order:", error);
-    }
-  });
+        $.ajax({
+          url: "/api/checkout/send-twilio-text",
+          method: "POST",
+          data: { orderDetails, orderTotal },
+          success: function (response) {
+            console.log("Twilio text sent:", response);
+          },
+          error: function (error) {
+            console.error("Error sending Twilio text:", error);
+          },
+        });
+      },
+      error: function (error) {
+        console.error("Error submitting order:", error);
+      },
+    });
 
-      // Show loading animation
-  $(".cart-container").empty();
-  $(".cart-container").append(`<p class="msg">Submitting your order...</p>`);
-
-  // Simulate a 2-second delay using setTimeout
-  setTimeout(() => {
-    // Clear the cart
+    // Show loading animation
     $(".cart-container").empty();
+    $(".cart-container").append(`<p class="msg">Submitting your order...</p>`);
 
-    // Display success message
-    $(".cart-container").append(`
-      <p class="msg">Your order has been submitted successfully!</p>
+    // Simulate a 2-second delay using setTimeout
+    setTimeout(() => {
+      // Clear the cart
+      $(".cart-container").empty();
+
+      // Display success message
+      $(".cart-container").append(`
+      <p class="msg1">Your order has been submitted successfully!</p>
       <button class='close'>Close</button>
     `);
 
-    // Attach event listener to close button
-    $(".close").on("click", () => {
-      // Hide the cart and revert to previous state
-      $(".cart-container").empty();
-    });
-  }, 2000);
+      // Attach event listener to close button
+      $(".close").on("click", () => {
+        // Hide the cart and revert to previous state
+        $(".cart-container").empty();
+
+        $(".cart-container").append(`
+      <p class="msg2">Your order will be ready for pickup in 10 mins!</p>
+      <button class='close2'>Close</button>
+    `);
+
+        $(".close2").on("click", () => {
+          $(".cart-container").empty();
+        });
+      });
+    }, 2000);
   });
 };
