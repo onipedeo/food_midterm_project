18651b8aab698be80aad037760bca2b8fda8760e
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-08-23 21:50:53 -0400

    delete estimated_completion from orders_dishes

--- db/schema/04_orders_dishes.sql
+++ db/schema/04_orders_dishes.sql
@@ -2,6 +2,5 @@ DROP TABLE IF EXISTS orders_dishes CASCADE;
 CREATE TABLE orders_dishes (
   order_id INT REFERENCES orders(id) ON DELETE CASCADE,
   dish_id INT REFERENCES dishes(id) ON DELETE CASCADE,
-  quantity INT NOT NULL DEFAULT 0,
-  estimated_completion TIME
+  quantity INT NOT NULL DEFAULT 0
 );
