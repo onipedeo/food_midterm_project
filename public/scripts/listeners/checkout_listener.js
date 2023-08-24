16313f44c5029f8b9c1dd46d9bac2b9aad5b8157
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-08-23 23:58:21 -0400

    refresh time on cart checkout. interval

--- public/scripts/listeners/checkout_listener.js
+++ public/scripts/listeners/checkout_listener.js
@@ -11,43 +11,53 @@ export const checkoutListener = () => {
       orderDetails: JSON.stringify(orderDetails), // Convert to JSON string
     };
 
-    //$.ajax({
-    //  url: "/api/checkout/submit-order",
-    //  method: "POST",
-    //  data: orderData,
-    //  success: function (response) {
-    //    console.log("Order submitted:", response);
-    //
-    //    $.ajax({
-    //      url: "/api/checkout/send-twilio-text",
-    //      method: "POST",
-    //      data: { orderDetails, orderTotal },
-    //      success: function (response) {
-    //        console.log("Twilio text sent:", response);
-    //      },
-    //      error: function (error) {
-    //        console.error("Error sending Twilio text:", error);
-    //      },
-    //    });
-    //  },
-    //  error: function (error) {
-    //    console.error("Error submitting order:", error);
-    //  },
-    //});
+    $.ajax({
+      url: "/api/checkout/submit-order",
+      method: "POST",
+      data: orderData,
+      success: function (response) {
+        $.ajax({
+          url: "/api/checkout/send-twilio-text",
+          method: "POST",
+          data: { orderDetails, orderTotal },
+          success: function (response) {
+            console.log("Twilio text sent:", response);
+          },
+          error: function (error) {
+            console.error("Error sending Twilio text:", error);
+          },
+        });
+      },
+      error: function (error) {
+        console.error("Error submitting order:", error);
+      },
+    });
+
+    const formatTimeAmPm = (time24h) => {
+      let splitTime = time24h.split(":");
+      let hours = Number(splitTime[0]);
+      let minutes = Number(splitTime[1]);
+
+      const amPm = hours >= 12 ? "PM" : "AM";
+      if (hours > 12) {
+        hours -= 12;
+      }
+      return `${hours}:${minutes} ${amPm}`;
+    };
 
     // Get estimated time and add to DOM
     const updateEstimatedTime = () => {
       $.get("/api/checkout/get-estimated-time", function (data) {
-        console.log("data", data);
+
         const estimatedTime = data.estimated_completion;
-        console.log("estimatedTime", estimatedTime);
+
         if (estimatedTime !== null) {
-          $(".cart-container .msg2").text(
-            `Your order will be ready for pickup at ${estimatedTime}`
+
+          const formattedTime = formatTimeAmPm(estimatedTime);
+
+          $(".msg2").text(
+            `Your order will be ready for pickup at ${formattedTime}`
           );
-          if (refreshInterval) {
-            clearInterval(refreshInterval);
-          }
         }
       });
     };
@@ -66,8 +76,7 @@ export const checkoutListener = () => {
       <p class="msg2">Waiting for estimated time of completion</p>
       <button class='close'>Close</button>
     `);
-      const refreshEstimate = setInterval(updateEstimatedTime(), 500);
-
+      let refreshEstimate = setInterval(updateEstimatedTime, 5000);
 
       // Attach event listener to close button
       $(".close").on("click", () => {
