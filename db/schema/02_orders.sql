b3da2dfeee442dc46beff0a380ae2f5b18603529
Nathan Wiles <83792456+nathanwiles@users.noreply.github.com>
2023-08-23 22:58:29 -0400

    add estimated_completion make orders_date timestamptz

--- db/schema/02_orders.sql
+++ db/schema/02_orders.sql
@@ -2,7 +2,8 @@ DROP TABLE IF EXISTS orders CASCADE;
 CREATE TABLE orders (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INT REFERENCES users,
-  orders_date DATE NOT NULL DEFAULT NOW(),
+  orders_date TIMESTAMPTZ  NOT NULL DEFAULT NOW(),
   orders_total INT NOT NULL,
+  estimated_completion TIME Default NULL,
   order_complete BOOLEAN NOT NULL DEFAULT FALSE
 );
