1d31c40bb33e364d0dc6417462d0df0128749e85
Andrew Li <130577102+Andrew-Li-12138@users.noreply.github.com>
2023-08-24 21:31:42 -0400

    implement refresh feature to employee side

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,10 +2,26 @@ import { functions } from './functions/functions_index.js';
 import { loadOrder, render_orders } from './functions/render_orders.js';
 import { listeners } from './listeners/listeners_index.js';
 
-
 $(document).ready(function() {
-   $(".login-form").hide();
-   $(".employee-content").hide();
+  // Function to load and render orders - employee side
+  function loadAndRenderOrders() {
+    $.ajax({
+      url: "/api/orders",
+      method: "GET",
+      success: (result) => {
+        const ordersHtml = loadOrder(result);
+        render_orders(ordersHtml);
+        listeners.submitTimeListener();
+      },
+      error: function(err) {
+        console.log("Error in Fetch Orders Data ", err);
+      },
+    });
+  }
+
+  //login setup
+  $(".login-form").hide();
+  $(".employee-content").hide();
   listeners.viewOrderListener();
 
   $(".show-login-form").click(function() {
@@ -13,40 +29,34 @@ $(document).ready(function() {
     $(".show-login-form").hide();
   });
 
+  //customer side
   $.ajax({
     url: "/api/widgets",
     method: "GET",
     success: (result) => {
       functions.render_menu(result);
       listeners.addToCartListener();
-
     },
     error: function(err) {
       console.log("Error in Fetch Dishes Data ", err);
     },
   });
 
-  //function to trigger when user clicks checkout button
+  // Function for "Check New Order" button
+  function refreshButton() {
+    $('#refreshButton').on('click', function() {
+    $('#orders-container-employee').empty();
+      loadAndRenderOrders();
+    });
+  }
+
+  // Initial load of orders
+  loadAndRenderOrders();
+  // call loadAndRenderOrders() again when click "Check New Order" button
+  refreshButton();
+
+
   listeners.checkoutListener();
 
-  //Employee ajax call
-  $.ajax({
-    url: "/api/orders",
-    method: "GET",
-    success: (result) => {
-
-      const ordersHtml = loadOrder(result);
-      render_orders(ordersHtml);
-      listeners.submitTimeListener();
-
-    },
-    error: function(err) {
-      console.log("Error in Fetch Orders Data ", err);
-    },
-  });
-
-
   console.log("loaded");
-
-
 });
